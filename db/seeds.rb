244c78df0bfb2e6622796cbbd7772cef653bb615
nkizito92 <nkizito57@gmail.com>
2020-12-26 10:35:52 -0600

    also changed the adjusted the key for the new attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 donny = Driver.create(first_name: "Donny", last_name: "Don")
-james = Client.create(first_name: "James", last_name: "Don", pay_rate: 50.0)
+james = Client.create(full_name: "James Don", pay_rate: 50.0)
 punchIn = Punch.create(client_id: 1, clock_in: Time.now)
 
 donny.clients << james
