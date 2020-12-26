e43f86b9f984d80ac3427d75cbf8d6c5c3984431
nkizito92 <nkizito57@gmail.com>
2020-12-26 10:35:03 -0600

    changed name attribute

--- db/migrate/20201212151348_create_clients.rb
+++ db/migrate/20201212151348_create_clients.rb
@@ -1,8 +1,7 @@
 class CreateClients < ActiveRecord::Migration[6.0]
   def change
     create_table :clients do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :full_name
       t.float :pay_rate
       t.integer :driver_id
 
