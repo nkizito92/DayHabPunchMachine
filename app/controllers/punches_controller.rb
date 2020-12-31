ba224315fecaa626fa73c20acaa5d738bc1eb14c
nkizito92 <nkizito57@gmail.com>
2020-12-31 17:11:17 -0600

    fix toggle bug for punches

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -1,5 +1,5 @@
 class PunchesController < ApplicationController
-
+    before_action :sleeping, only: [:create, :destroy]
     def index
         punches = Punch.all
         render json: punches, include: [:client]
@@ -19,15 +19,17 @@ class PunchesController < ApplicationController
         # need to be logged in with admin 
         # find client by name
         client = Client.find_by(full_name: params[:punch][:full_name])
-        if client.punches.length.odd?
+        client_punches = client.punches.length
+        client_punches = 1 if client_punches === 0 
+        # punch_in twice for no reason before punching Out
+        if client_punches.odd?
             punched = client.punches.build({client_id: client.id, clock_in: Time.new.strftime("%l:%M %p")})
         else 
             punched = client.punches.build({client_id: client.id, clock_out: Time.new.strftime("%l:%M %p")})
         end 
             
         punched.save
-        sleep(2.3)
-        render json: punched
+        render json: punched, include: [:client]
         # if the client_id match with the clients id and clock_in is not nil 
         
             # update clock_out
@@ -36,6 +38,12 @@ class PunchesController < ApplicationController
 
     def clock_out 
         # need to be logged in with admin 
+    end
+
+    private
+
+    def sleeping
+        sleep(2.2)
     end
 
 end
