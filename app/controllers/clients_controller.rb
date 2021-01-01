bbd023de05824c74e3cbe79f8e5261f12b85e497
nkizito92 <nkizito57@gmail.com>
2020-12-31 22:49:55 -0600

    added line of code to create a client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,14 +8,12 @@ class ClientsController < ApplicationController
     def show
         james = Client.find_by_id(params[:id])
         # render json: james {id: params[:id]}
-        render json: james 
-        # render json: james, include: [:chat, :guest]
+        render json: james, include: [:punches, :driver] 
     end     
 
     def create
         # need to be logged in with admin 
-        client = Client.create(full_name: params[:full_name], pay_rate: params[:pay_rate])
-
+        client = Client.create(client_params)
         render json: client, include: [:driver, :punches]
     end
 
@@ -34,7 +32,7 @@ class ClientsController < ApplicationController
     
     private
     def client_params
-        params.require("client").permit(:id, :full_name, :pay_rate, :driver_id)
+        params.require("client").permit(:id, :full_name, :pay_rate, :driver_id, :driver)
     end
     
     def sleeping
