54b9d139ae49f598e1f6b789177a7cdc3582eefc
nkizito92 <nkizito57@gmail.com>
2021-02-11 18:12:49 -0600

    fixed the json rending all the data including associate

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -1,5 +1,5 @@
 class PunchesController < ApplicationController
-    before_action :sleeping, only: [:create, :destroy]
+    before_action :sleeping, only: [ :destroy, :update]
     def index
         punches = Punch.all
         render json: punches, include: [:client]
@@ -7,43 +7,51 @@ class PunchesController < ApplicationController
 
     def show
         james = Punch.find_by_id(params[:id])
-        render json: james
+        render json: james, include: [:client]
     end
 
-    def new
-        # need to be logged in with admin 
-        @punch = Punch.new
-    end
     
-    def create 
-        # need to be logged in with admin 
-        # find client by name
-        client = Client.find_by(full_name: params[:punch][:full_name])
-        client_punches = client.punches.length
-        client_punches = 1 if client_punches === 0 
-        # punch_in twice for no reason before punching Out
-        if client_punches.odd?
-            punched = client.punches.build({client_id: client.id, clock_in: Time.new.strftime("%l:%M %p")})
+    def create        
+        client = Client.find_by(full_name: params[:punched][:full_name])
+        clocked = ""
+        if !client.nil?
+            client_punches = client.punches.length + 1
+            if client_punches.odd?
+                clocked = "Clocked In!"
+                punched = client.punches.build({client_id: client.id, clock_in: Time.new.strftime("%H%M"), date: Time.new.strftime("%B"), month_day: Time.new.strftime("%b %e")})
+            else 
+                clocked = "Clocked Out!"
+                punched = client.punches.build({client_id: client.id, clock_out: Time.new.strftime("%H%M"), date: Time.new.strftime("%B"), month_day: Time.new.strftime("%b %e")})
+            end 
+            punched.save
+        end
+        if !punched.nil?
+            render json: {punched: punched, message: {success: "#{Time.new.strftime("%l:%M %p")} #{client.full_name} #{clocked}", error: ""}}, include: [:client]
         else 
-            punched = client.punches.build({client_id: client.id, clock_out: Time.new.strftime("%l:%M %p")})
-        end 
-            
-        punched.save
-        render json: punched, include: [:client]
-        # if the client_id match with the clients id and clock_in is not nil 
-        
-            # update clock_out
-        # else create punch in
+            render json: {punched: punched, message: {success: "", error: "Client Doesn't Exist!"}}, include: [:client]
+        end
     end 
 
-    def clock_out 
-        # need to be logged in with admin 
+    def update
+        punch = Punch.find_by_id(params[:id])
+        punch.update(punch_params)
+        render json: punch, include: [:client]
+    end
+
+    def destroy 
+        punch = Punch.find_by_id(params[:id])
+        punch.delete if !punch.nil?
+        render json: punch, include: [:client]
     end
 
     private
 
     def sleeping
         sleep(2.2)
+    end
+
+    def punch_params
+        params.require("punch").permit(:id, :clock_in, :clock_out, :date, :month_day, :client_id, :client )
     end
 
 end
