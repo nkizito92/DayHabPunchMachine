a135303dd41246ffc0bf4ec8dd2450dae3cf1d2d
nkizito92 <nkizito57@gmail.com>
2021-02-11 18:05:33 -0600

    controller actions have been added

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,21 +1,39 @@
 class DriversController < ApplicationController
+    before_action :sleeping, only: [:create, :destory, :update]
     def index
-
+        drivers = Driver.all
+        render json: drivers, include: [:clients, :user]
     end
 
     def show 
-
+        driver = Driver.find_by_id(params[:id])
+        render json: driver, include: [:clients, :user]
     end 
-
-    def new
-        # need to be logged in with admin 
-    end
 
     def create
-        # need to be logged in with admin 
+        driver = Driver.create(driver_params)
+        render json: driver, include: [:clients, :user]
     end 
 
-    def delete
-        # need to be logged in with admin 
+    def update 
+        driver = Driver.find_by_id(driver_params[:id])
+        driver.update(first_name: driver_params[:first_name], last_name: driver_params[:last_name])
+        render json: driver, include: [:clients, :user]
+    end 
+
+    def destroy
+        driver = Driver.find_by_id(params[:id])
+        driver.delete
+        render json: driver, include: [:clients, :user]
+    end
+
+    private 
+
+    def driver_params
+        params.require('driver').permit(:id, :first_name, :last_name, :clients, :user, :user_id)
+    end
+
+    def sleeping
+        sleep(2.2)
     end
 end
