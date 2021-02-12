15ae9fc7787c9b7381547029af01bbdbb0fb1b6b
nkizito92 <nkizito57@gmail.com>
2021-02-11 18:15:41 -0600

    users controller has the full CRUD actions set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,33 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: user
+        render json: users, include: [:drivers]
     end
 
     def show
         @user = User.find_by_id(params[:id])
-        render json: @user
+        render json: @user, include: [:drivers]
     end
 
     def create 
         @user = User.new(user_params)
         if @user.save
             login!
-            render json: @user
+            render json: @user, include: [:drivers]
         end
+    end
+
+    def update
+        user = User.find_by(username: user_params[:username])
+        if user && user.authenticate(user_params[:old_password])
+            user.update(user_params)
+        end 
+        if user.authenticate(user_params[:password_confirmation]) && user.authenticate(user_params[:password])
+            render json: {user: user, message: {success: "#{user.username} Password Updated!!", error: ""}}, include: [:drivers]
+        else 
+            render json: {message: {success: "", error: "Password do not match try again!"}}
+        end
+        sleep(2.3)
     end
 
     private
