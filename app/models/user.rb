f50884fdb0870a5be37f8fefed2f9c29d6488238
nkizito92 <nkizito57@gmail.com>
2021-02-11 18:14:19 -0600

    user has many drivers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
+    has_many :drivers
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :username, length: { minimum: 4 }
