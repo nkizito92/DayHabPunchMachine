1c1107344f238b0b6114b74763633b579a1f76c1
nkizito92 <nkizito57@gmail.com>
2021-04-25 23:30:46 -0500

    changed localhost port to 3001

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  default_url_options :host => "localhost:3001"
   post '/login',    to: 'sessions#create'
   post '/logout',   to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
-
   resources :users, only: [:create, :show, :index, :update]
   resources :clients
   resources :drivers
   resources :punches
+  resources :image_elements, only: [:index, :destroy, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
